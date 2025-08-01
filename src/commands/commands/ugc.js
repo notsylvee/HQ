69c8172f7cc22e38eee4067a22bf960a7ce672ec
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-31 23:45:20 -0400

    + /ugc

---
+++ src/commands/commands/ugc.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs/promises");
+
+module.exports = {
+    data: {
+        name: "ugc",
+        description: "Get a random Pressure UGC",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+      const ugcsJsonData = await fs.readFile("data/ugc.json", {encoding: "utf8"});
+      const ugcsMap = JSON.parse(ugcsJsonData);
+      const ugcs = ugcsMap["ugc"];
+      const ugc = ugcs[Math.floor(Math.random() * ugcs.length)];
+
+      const embed = new EmbedBuilder()
+      .setColor(`#bbc3cd`)
+      .setTitle(`${ugc.name}`)
+      .setDescription(`${ugc.description}`)
+      .setThumbnail(`https://cdn.sylvee.xyz/pressurejumpugc${ugc.num}.png`)
+      .addFields({ name: '\u200b', value: `<:Robux:1400684315674935346> ${ugc.price}` })
+
+      await interaction.reply({ embeds: [embed] });
+    },
+};
